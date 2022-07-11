ffb1fd638cb2563dcf4f27ac0f497aba89a42198
Maria <maria.caram@gmail.com>
2022-07-11 16:03:55 -0300

    Se crea logica para guardar productos en un estado

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,20 @@ import Inicio from "./components/pages/Inicio"
 import ListaProductos from './components/productos/ListaProductos';
 import Login from './components/pages/Login';
 import Store from './components/pages/Store';
+import { useState, useEffect } from 'react';
 function App() {
+
+const [productos, setProductos] = useState ([])
+useEffect (()=>{consultarApi()} , []);
+
+const consultarApi = async() => {try {
+const respuesta = await fetch ("http://localhost:3003/productos")
+const dato =  respuesta.json()
+console.log (dato)
+setProductos(dato)} catch(error) {
+console.log (error)
+}
+}
   return (
     <Router>
       <Navigation></Navigation>
@@ -16,7 +29,7 @@ function App() {
         <Route exact path = "/" element= {<Inicio></Inicio>}></Route>
         <Route exact path = "/store" element= {<Store></Store>}></Route>
 
-        <Route exact path = "/adminBoard" element= {<ListaProductos></ListaProductos>}></Route>
+        <Route exact path = "/adminBoard" element= {<ListaProductos productos={productos}></ListaProductos>}></Route>
         <Route exact path = "/login" element= {<Login></Login>}></Route>
       </Routes>
       <Footer></Footer>
--- src/components/productos/ListaProductos.js
+++ src/components/productos/ListaProductos.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { Container, ListGroup, Button } from "react-bootstrap";
-import ItemProducto from "./ItemProducto";
+import ItemProducto from "./ItemProducto"
 import {Link} from 'react-router-dom'
-const ListaProductos = () => {
+
+const ListaProductos = (props) => {
   return (
     <Container className="my-5">
       <h1 className="text-center mb-5">Lista de productos</h1>
       <hr />
       <article className="d-flex align-items-center mb-4 justify-content-end">
         <p className="my-0 me-4 fw-bold">Agregar nuevos productos: </p>
-<Link to="/nuevo" className="btn btn-primary">Agregar</Link>
+{/* <Link to="/nuevo" className="btn btn-primary">Agregar</Link> */}
       </article>
       <ListGroup>
-
-
+        console.log(productos)
+{/* {props.productos.map ((producto)=><ItemProducto key= {producto.id}></ItemProducto>)} */}
       </ListGroup>
     </Container>
   );
